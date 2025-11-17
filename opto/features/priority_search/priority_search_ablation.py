017e0309f3f11659905eaadb7f2941d66b56a04e
xuanfeiren <xuanfeir@gmail.com>
2025-11-16 22:39:52 -0500

    use eps0.1

--- opto/features/priority_search/priority_search_ablation.py
+++ opto/features/priority_search/priority_search_ablation.py
@@ -1075,7 +1075,7 @@ class EpsilonNetPS(PrioritySearch):
     A subclass of PrioritySearch, which keeps an epsilon-net as the memory. Reject new candidates that are in the epsilon-net of the memory.
     """
     def __init__(self,
-                 epsilon: float = 0.01,
+                 epsilon: float = 0.1,
                  use_summarizer: bool = False,
                  *args,
                  **kwargs):
